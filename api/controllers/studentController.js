3a842b87a1c6c36a30c61b5ce19ce5362e879290
cwan <manwan_444@hotmail.com>
2017-11-19 19:06:02 +0700

    edit firebase

--- api/controllers/studentController.js
+++ api/controllers/studentController.js
@@ -1,24 +1,7 @@
 "use strict";
 
-/*Initialize Firebase*/
-var admin = require("firebase-admin"),
-    serviceAccount = {
-        "type": process.env.FIREBASE_TYPE,
-        "project_id": process.env.FIREBASE_PROJECT_ID,
-        "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-        "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-        "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-        "client_id": process.env.FIREBASE_CLIENT_ID,
-        "auth_uri": process.env.FIREBASE_AUTH_URI,
-        "token_uri": process.env.FIREBASE_TOKEN_URI,
-        "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-        "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
-    };
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://auopenhouse-wvn.firebaseio.com"
-});
+/*Firebase*/
+var firebase = require("../firebase");
 
 exports.Authentication = function(req, res, next) {
     if (req.session.sid) {
@@ -62,7 +45,7 @@ exports.login = function(req, res, next) {
         return;
     }
 
-    admin.auth().verifyIdToken(req.body.idToken)
+    firebase.auth().verifyIdToken(req.body.idToken)
         .then(function(decodedToken) {
             var data = {
                 sid: decodedToken.uid,
